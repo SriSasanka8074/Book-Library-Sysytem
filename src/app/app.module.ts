928da24781cf21228bb8f2ccfcb8ff4d2d20ccdf
Ranjith Rana Rudra <srisasanka096@gmail.com>
2020-02-20 03:17:55 +0530

    changes commited by sasanka 20-02-2020 03:17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,15 @@ import { BookComponent } from './book/book.component';
 import { ManageBooksComponent } from './manage-books/manage-books.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
-    ManageBooksComponent
+    ManageBooksComponent,
+    // SearchPipe,
   ],
   imports: [
     BrowserModule,
@@ -20,6 +23,7 @@ import {HttpClientModule} from '@angular/common/http';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/manage-books/manage-books.component.ts
+++ src/app/manage-books/manage-books.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import {faEdit} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-manage-books',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageBooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
 
-  ngOnInit(): void {
+  manageBooksForm: FormGroup;
+  booksData: Array<any> = [];
+  title = 'Manage Books';
+  faEye = faEye;
+  faEdit = faEdit;
+
+  ngOnInit() {
+    this.createFormGroup();
+    this.getBooksList();
   }
 
+  createFormGroup() {
+    this.manageBooksForm = this.formBuilder.group({
+      fromDate: this.formBuilder.control('', []),
+      toDate: this.formBuilder.control('', []),
+    });
+  }
+
+  getBooksList() {
+    const url = '';
+    const obj: any = this.manageBooksForm.value;
+    this.httpClient.get(url).subscribe(res => {
+      if (res) {
+        const data: any = res;
+        this.booksData = data;
+      } else {
+        this.booksData = [];
+      }
+    }, error => {
+      // this.booksData = [];
+      this.booksData = [
+        {
+          id: 1,
+          bookName: 'afafafafa',
+          author: 'dvsgsgsgsg'
+        }
+      ];
+    });
+  }
+
+  navigateToBookMaster(status: boolean, id: number) {
+    if (status) {
+      this.router.navigate(['/book', 'view', id]);
+    } else {
+      this.router.navigate(['/book', 'edit', id]);
+    }
+  }
+
+  /* search(value: string) {
+    if (value && value.length > 0) {
+
+    }
+  } */
 }
